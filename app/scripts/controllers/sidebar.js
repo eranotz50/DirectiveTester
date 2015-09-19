12f40d0e8547b855936a8de510c6c6da36c0957c
eranotz50 <eranotz50@gmail.com>
2015-09-19 23:22:23 +0300

    nav bar re-style

--- app/scripts/controllers/sidebar.js
+++ app/scripts/controllers/sidebar.js
@@ -6,9 +6,37 @@
     angular.module('directivesTesterApp')
         .controller('SideBarCtrl', function ($scope) {
 
-            $scope.links = [{ title: "module 1" , state: "main"},
+           /* $scope.links = [{ title: "module 1" , state: "main"},
                 { title: "Directives Fundamentals" , state: "directivesFnd"}
             ];
+*/
+
+            $scope.links = [
+                {
+                    title : "Directives Fundamentals",
+                    state: "directivesFnd",
+                    subLinks: [{
+                        title : "Module 1",
+                        state : "module1"
+                    },
+                    {
+                        title : "Module 2",
+                        state : "module2"
+                    }]
+                },
+                {
+                    title : "Authorization",
+                    state: "authorization",
+                    subLinks: [{
+                        title : "Module 1",
+                        state : "module1"
+                    },
+                        {
+                            title : "Module 2",
+                            state : "module2"
+                        }]
+                }
+            ]; // end links
 
         });
 })();
